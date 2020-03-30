d9e6083798db9a55b89ad3c8982eaf7d315779a3
Cgameworld <support@cgameworld.com>
2020-03-29 19:02:22 -0700

    collision fix for Sunset Harbor

---
+++ FineRoadAnarchy/PrefabUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FineRoadAnarchy
+{
+    public class PrefabUtil
+    {
+        //from NoPillars
+        public static HashSet<NetInfo> GetAllPrefabs()
+        {
+            var result = new HashSet<NetInfo>();
+            foreach (var prefab in Resources.FindObjectsOfTypeAll<NetInfo>())
+            {
+                result.Add(prefab);
+            }
+            return result;
+        }
+    }
+}
